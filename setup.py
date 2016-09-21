59141043a379402698e0275de869a72cd0d8cf55
Jonas Ruebsam <jonas.ruebsam@gmail.com>
2016-09-21 15:13:26 +0200

    Add simple setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pypiv',
+      version='0.01',
+      description='Python Particle Image Velocimerty Library',
+      author='Jonas Ruebsam',
+      author_email='jonas.ruebsam@gmail.com',
+      packages=['pypiv', 'pypiv.piv'],
+     )
