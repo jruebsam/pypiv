81d082dd7df309ad8e9918951d0e35dc0a3028d1
Jonas Ruebsam <jonas.ruebsam@gmail.com>
2016-09-22 02:14:28 +0200

    Add postprocess doctstring.

--- pypiv/postprocess.py
+++ pypiv/postprocess.py
@@ -1,2 +1,13 @@
-def compute_coordinate_transformations(piv):
+def compute_coordinate_transformations(piv, width, height, dt):
+    ''''
+    Compute Coordinate system  transformation from
+          ------ y              y
+         |                      |
+         |             to       |
+         x                       ------ x
+
+    with x  ->  -y , u -> -v , y -> x, v -> u
+    and time scaling
+    '''
     pass
+
