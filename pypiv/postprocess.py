45ca31385795ef074a295e78951d4212a5d56b74
Kevin Luedemann <kevin27luedemann@gmail.com>
2016-11-10 15:37:22 +0100

    add funktions for coordinatetransform
    first funktion:
    switch u and v to represent normal (botom left) origin
    
    second funktion:
    switch from lower left to upper left origin for plotting in imagecoordinates

--- pypiv/postprocess.py
+++ pypiv/postprocess.py
@@ -28,3 +28,10 @@ def compute_coordinate_transformations(piv, width=100, dt=1.):
     U, V = piv.v.T/dt, -piv.u.T/dt
     return X, Y, U, V
 
+def switch_coordinates(piv):
+	u = piv.u
+	piv.u = piv.v
+	piv.v = u
+
+def plots_coordinates(u,v):
+	return u,-v
