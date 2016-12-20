f97c796a9c677075873047ed36c8bdc354d7e9ca
Jonas Ruebsam <jonas.ruebsam@gmail.com>
2016-12-20 14:36:57 +0100

    Add inheritance for AdaptivePIV from DirectPIV.

--- pypiv/piv/adaptive_piv.py
+++ pypiv/piv/adaptive_piv.py
@@ -8,12 +8,12 @@ from fft_correlator import FFTCorrelator
 from grid_spec import GridSpec
 from process import find_subpixel_peak
 
-class AdaptivePIV(object):
+class AdaptivePIV(DirectPIV):
     def __init__(self, piv_object, window_size, search_size, distance):
-
         image_a, image_b = np.copy(piv_object.frame_a), np.copy(piv_object.frame_b)
-        self.grid_spec = GridSpec(image_a.shape, image_a.strides,
-                                   window_size, search_size, distance)
+
+        super(AdaptivePIV, self).__init__(image_a, image_b, window_size,
+                                          search_size, distance)
 
         if self.grid_spec.equal_to(piv_object.grid_spec) == False:
             vs = VelocityUpscaler(self.grid_spec, piv_object.grid_spec)
@@ -21,38 +21,9 @@ class AdaptivePIV(object):
             self.v = vs.scale_field(piv_object.v)
         else:
             self.u, self.v = np.copy(piv_object.u), np.copy(piv_object.v)
+        self._deform_grid()
 
-        self._correlator = FFTCorrelator(window_size, search_size)
-        self._set_images(image_a, image_b)
-        self._grid_creator()
-
-    def _set_images(self, img_a, img_b):
-        '''Set the correlation images of the PIV algorithm'''
-        self.frame_a = img_a
-        pad = self.grid_spec.pad
-        self._padded_fb = np.pad(img_b, 2*(pad,), 'constant')
-        if self.grid_spec.pad == 0:
-            self.frame_b = self._padded_fb
-        else:
-            self.frame_b = self._padded_fb[pad:-pad, pad:-pad]
-
-    def _grid_creator(self):
-        shape_fa    = self.grid_spec.get_interogation_grid_shape()
-        strides_fa  = self.grid_spec.get_interogation_grid_strides()
-        self.grid_a = as_strided(self.frame_a, strides=strides_fa, shape=shape_fa)
-
+    def _deform_grid(self):
         grid_def    = GridDeformator(self.frame_b, self.grid_spec)
         self.grid_b = grid_def.create_deformed_grid(self.u, self.v)
 
-    def correlate_frames(self, method='gaussian'):
-        lx, ly, _, _ = self.grid_a.shape
-        for i, j in np.ndindex(self.grid_a.shape[:2]):
-            displacement = (self._correlator .get_displacement(self.grid_a[i, j],
-                                                               self.grid_b[i, j],
-                                                               subpixel_method=method))
-            self.u[i, j] += displacement[0]
-            self.v[i, j] += displacement[1]
-        return  self.u, self.v
-
-    def correlate_frames_2D(self):
-        self.correlate_frames(method='9point')
