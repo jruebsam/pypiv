7cb8f03c7a71cc7dcced382a08f311b102adcb46
Jonas Ruebsam <jonas.ruebsam@gmail.com>
2016-12-22 01:23:30 +0100

    Add first version of cubic interpolator.

--- pypiv/piv/grid_deformator.py
+++ pypiv/piv/grid_deformator.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy.lib.stride_tricks import as_strided
 from scipy.ndimage import map_coordinates
+import sys
 
 class GridDeformator(object):
     def __init__(self, frame, shape, distance):
@@ -44,8 +45,40 @@ class GridDeformator(object):
         out = np.zeros(v.shape+2*(self._window_size,))
         for i, j in np.ndindex(self._shape[:2]):
             p, q = ptsax[i, j].shape
+            '''
             sample = map_coordinates(self._frame,
                     [ptsax[i, j].flatten(), ptsay[i, j].flatten()], order=1).reshape(p, q)
             out[i,j] = sample
+            '''
+            print i, j
+            for  k in range(p):
+                for l in range(q):
+                    out[i, j, k, l] = self._test_cubic(ptsax[i, j, k, l], ptsay[i, j, k, l])
+            #if j>30:
+            #    sys.exit(1)
         return out
 
+    def _test_cubic(self, x, y):
+        pos_x, dx = divmod(x, 1)
+        pos_y, dy = divmod(y, 1)
+
+        pos = np.array([-2.5, -1.5, -0.5, 0.5, 1.5, 2.5])
+        x = np.abs(dx - pos)
+        y = np.abs(dy - pos)
+
+        M  = np.array([[-18./5.,  14./5.,     1.,     1.,  14./5., -18/5.],
+                       [ 21./5., -27./5.,     0.,     0., -27./5., 21./5.],
+                       [ -8./5.,  16./5., 11./5., 11./5.,  16./5., -8./5.],
+                       [  1./5.,  -3./5.,  6./5.,  6./5.,  -3./5.,  1./5.]])
+
+        outx = M[0] + x*M[1] + x**2*M[2] + x**3*M[3]
+        outy = M[0] + y*M[1] + y**2*M[2] + y**3*M[3]
+        out  = np.outer(outx, outy)
+
+        frame_window = self._frame[pos_x-2:pos_x+4, pos_y-2:pos_y+4]
+
+        try:
+            out = np.sum(out*frame_window)
+        except:
+            out = 0
+        return out
