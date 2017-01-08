109eae1f6b5fd446a5d4552c894a464486cf5d83
Jonas Ruebsam <jonas.ruebsam@gmail.com>
2017-01-08 22:05:40 +0100

    Add cython installer.

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+ext_modules = [ Extension("pypiv.piv.interpolator", [ "pypiv/piv/interpolator.pyx" ])]
+cmdclass = { }
+cmdclass.update({ 'build_ext': build_ext })
 
 setup(name='pypiv',
       version='0.01',
@@ -8,4 +14,6 @@ setup(name='pypiv',
       author='Jonas Ruebsam',
       author_email='jonas.ruebsam@gmail.com',
       packages=['pypiv', 'pypiv.piv'],
+      cmdclass=cmdclass,
+      ext_modules=ext_modules,
      )
