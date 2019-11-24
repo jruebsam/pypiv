b468f7003461f8d1e6eef9e3eeca7ebba10cb5c2
Kevin Luedemann <kevin27luedemann@googlemail.com>
2019-11-24 11:51:32 +0100

    Preprocess with different bg subtraction

--- pypiv/preprocess.py
+++ pypiv/preprocess.py
@@ -14,6 +14,47 @@ def subtract_background2file(images, output_file):
         data = np.min(np.array([data, imread(img)]), axis=0)
     np.save(output_file, data)
 
+def subtract_background(images):
+    '''
+    Compute Background from image list and save it as numpy array
+    '''
+    readin  = False
+    if isinstance(images[0],str):
+        bg = imread(images[0])
+        readin = True
+    else:
+        bg = np.copy(images[0])
+    for  img in images:
+        if readin:
+            bg = np.min(np.array([bg, imread(img)]), axis=0)
+        else:
+            bg = np.min(np.array([bg, img]), axis=0)
+    imgs    = []
+    for img in images:
+        if readin:
+            imgs.append(imread(img)-bg)
+        else:
+            imgs.append(img-bg)
+    return np.array(imgs),bg
+
+def subtract_background_pair(images):
+    '''
+    Compute Background from image list and save it as numpy array
+    '''
+    readin  = False
+    if isinstance(images[0],str):
+        bg = np.min(np.array([imread(images[0]), imread(images[1])]), axis=0)
+    else:
+        bg = np.min(np.array([images[0], images[1]]), axis=0)
+    imgs    = []
+    for img in images:
+        if readin:
+            imgs.append(imread(img)-bg)
+        else:
+            imgs.append(img-bg)
+    return np.array(imgs),bg
+
+
 def cap_image(img, cap_min, cap_max):
     img[img<cap_min] = cap_min
     img[img>cap_max] = cap_max
